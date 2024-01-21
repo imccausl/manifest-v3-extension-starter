ecb2f678518862a265f0844949ea622b62603d4c
Ian McCausland <imccausl@gmail.com>
2024-01-21 14:44:49 -0500

    feat: add react query

--- app/popup/Popup/index.tsx
+++ app/popup/Popup/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useCurrentTab } from '../../shared/hooks'
 
 export const Popup: React.FC = () => {
+  const { data } = useCurrentTab()
+
   return (
     <div>
       <h1>Hello, world!</h1>
+      {data ? <p>Current tab: {data.id}</p> : null}
     </div>
   )
 }
